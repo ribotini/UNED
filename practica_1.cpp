b998282383faee84e556209846b77077f838eaaa
ribotini <39949195+ribotini@users.noreply.github.com>
2020-11-13 09:21:34 +0100

    Impresión de datos personales
    Escribe tus datos personales en la cabecera y en la parte ejecutiva sin modificar la estructura del programa.
    El formato de corchetes de la cabecera es imprescindible para la corrección del programa.

---
+++ practica_1.cpp
@@ -0,0 +1,19 @@
+/******************************
+* NOMBRE: #Nombre#
+* PRIMER APELLIDO: #Apellido1#
+* SEGUNDO APELLIDO: #Apellido2#
+* DNI: #123456789#
+* EMAIL: #usuario@alumno.uned.es#
+******************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("Nombre\n");
+    printf("Apellido1\n");
+    printf("Apellido2\n");
+    printf("123456789\n");
+    printf("usuario@alumno.uned.es");
+
+    return 0;
+}
